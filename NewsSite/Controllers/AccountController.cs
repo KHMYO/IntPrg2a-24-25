ce7a04962941cd16a868e0433d7e51f6857c5b7d
khmyo <haltasodev@gmail.com>
2025-05-05 10:15:27 +0300

    Login logic done

--- NewsSite/Controllers/AccountController.cs
+++ NewsSite/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using NewsSite.Models;
 using NewsSite.Models.ViewModels;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -9,6 +10,14 @@ namespace NewsSite.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly MyContext context;
+
+        public AccountController()
+        {
+            context = new MyContext();
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -21,21 +30,27 @@ namespace NewsSite.Controllers
 
 
         [HttpPost]
-        public async Task<IActionResult> Login(StaffLoginViewModel data) {
+        public async Task<IActionResult> Login(StaffLoginViewModel data)
+        {
 
             try
             {
                 data.ReturnUrl = "";
                 if (ModelState.IsValid)
                 {
-                    if (data.Username == "admin" && data.Password == "123456")
+
+                    var users= context.Staff.ToList();  
+                    var properUser= users.FirstOrDefault(x => x.UserName == data.Username && x.Password == data.Password);
+
+                    if ( properUser is not null /*data.Username == "admin" && data.Password == "123456"*/)
                     {
                         // Cookie oluşturma
                         var claims = new List<Claim>
                         {
                             new Claim(ClaimTypes.Name, data.Username),
                             //new Claim(ClaimTypes.Role, "Admin")
-                            new Claim("Id","10")
+                            new Claim("Id",properUser.Id.ToString()),
+                            new Claim("Fullname", properUser.Name),
                         };
                         var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
                         var authProperties = new AuthenticationProperties
@@ -62,8 +77,16 @@ namespace NewsSite.Controllers
 
 
             return View(data);
-        
+
         }
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
 
 
 
